165826bd169b4eac14e25cb0107572c8c8aeb5c5
xa4ax2000 <andrew.hyun@alumni.stonybrook.edu>
2016-07-25 06:32:53 -0400

    Add files via upload

---
+++ CW/CW.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << endl;
+	}
+
+	int i = 0
+	while (i < 10)
+	{
+		cout << i << endl;
+		i++;
+	}
+	int i = 0;
+	while (i != 0)
+	{
+		cout << "i is equal to " << i << endl;
+		i++;
+	}
+	cout << "Done!" << endl;
+
+	int i = 0;
+	while (i != 10)
+		cout << "i is equal to " << i << endl;
+		i++;
+}
---
+++ cw2/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i = 0;
+
+	do
+	{
+		cout << i << endl;
+		i++;
+	} while (i < 0);
+
+	int y = 0;
+	while (y < 0)
+	{
+		cout << y << endl;
+		y++;
+	}
+
+
+
+	system("pause");
+	return 0;
+}
